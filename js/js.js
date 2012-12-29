f7a3f7227aae3ba38e001d0e6a3e915b1ccd4c6b
Hélio Oliveira <insign@gmail.com>
2012-12-29 00:24:35 -0300

    Use cordova media api

--- js/js.js
+++ js/js.js
@@ -7,19 +7,6 @@ jQuery(document).ready(function($) {
 
   _.wind.on('resize', _e.resize);
 
-//  jPlayer
-  $("#player").jPlayer({
-    ready: function(e) {
-	 $(this).jPlayer("setMedia", {
-	   mp3: "audio/strobe.mp3",
-	   oga: "audio/strobe.ogg"
-	 });
-    },
-    swfPath: "js/jplayer",
-    supplied: "mp3, oga",
-    preload: 'auto'
-  });
-
 });
 
 _e = {
@@ -29,3 +16,85 @@ _e = {
     });
   }
 };
+
+// Wait for Cordova to load
+//
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// Cordova is ready
+//
+function onDeviceReady() {
+  playAudio("http://audio.ibeat.org/content/p1rj1s/p1rj1s_-_rockGuitar.mp3");
+}
+
+// Audio player
+//
+var my_media = null;
+var mediaTimer = null;
+
+// Play audio
+//
+function playAudio(src) {
+  // Create Media object from src
+  my_media = new Media(src, onSuccess, onError);
+
+  // Play audio
+  my_media.play();
+
+  // Update my_media position every second
+  if (mediaTimer == null) {
+    mediaTimer = setInterval(function() {
+	 // get my_media position
+	 my_media.getCurrentPosition(
+		    // success callback
+				  function(position) {
+				    if (position > -1) {
+					 setAudioPosition((position) + " sec");
+				    }
+				  },
+				  // error callback
+						function(e) {
+						  console.log("Error getting pos=" + e);
+						  setAudioPosition("Error: " + e);
+						}
+				  );
+				}, 1000);
+		}
+}
+
+// Pause audio
+//
+function pauseAudio() {
+  if (my_media) {
+    my_media.pause();
+  }
+}
+
+// Stop audio
+//
+function stopAudio() {
+  if (my_media) {
+    my_media.stop();
+  }
+  clearInterval(mediaTimer);
+  mediaTimer = null;
+}
+
+// onSuccess Callback
+//
+function onSuccess() {
+  console.log("playAudio():Audio Success");
+}
+
+// onError Callback
+//
+function onError(error) {
+  alert('code: ' + error.code + '\n' +
+		'message: ' + error.message + '\n');
+}
+
+// Set audio position
+//
+function setAudioPosition(position) {
+  document.getElementById('audio_position').innerHTML = position;
+}
