82aa575b0a5ef6ff840463680da203356b41779d
Hélio Oliveira <insign@gmail.com>
2012-12-30 01:15:38 -0300

    Small changes

--- autobuild.ps1
+++ autobuild.ps1
@@ -1,8 +1,15 @@
+# Made by Helio <insign@gmail.com> to One Focus
+
+$project_folder  = "D:\Dropbox\Sites\onefocus"
+$url_after = "https://build.phonegap.com/apps/270320/push"
+
+################################################################################
+
+cd $project_folder
 git add -A
 [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
 $commit_msg = [Microsoft.VisualBasic.Interaction]::InputBox("What's the news?", "Commit message", "Small changes")
 $commit_msg
 git commit -a -m $commit_msg
 git push
-#Invoke-Expression "cmd.exe /C start https://build.phonegap.com/apps/270320/builds"
-Invoke-Expression "cmd.exe /C start https://github.com/insign/OneFocus"
+Invoke-Expression "cmd.exe /C start $url_after"
