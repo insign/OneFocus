e132bac5320827e6ac2154ef8d0a271c48e33969
Hélio Oliveira <insign@gmail.com>
2012-12-31 00:05:41 -0300

    tests

--- autobuild.ps1
+++ autobuild.ps1
@@ -11,11 +11,10 @@ $url_after = "https://build.phonegap.com/apps/270320/push"
 clear
 
 # Ask about the commit message (too slow)
-#[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
-#$commit_msg = [Environment]::GetEnvironmentVariable("last_commit_msg","User")
-$commit_msg = "tests";
+[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+$commit_msg = [Environment]::GetEnvironmentVariable("last_commit_msg","User")
 $commit_msg = [Microsoft.VisualBasic.Interaction]::InputBox("What's the news?", "Commit message", $commit_msg)
-#[Environment]::SetEnvironmentVariable("last_commit_msg", $commit_msg, "User")
+[Environment]::SetEnvironmentVariable("last_commit_msg", $commit_msg, "User")
 
 # Open the folder
 cd $project_folder
