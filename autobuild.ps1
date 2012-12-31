504c206884e727d8769339d2156664dedca70c35
Hélio Oliveira <insign@gmail.com>
2012-12-30 23:56:47 -0300

    tests

--- autobuild.ps1
+++ autobuild.ps1
@@ -1,8 +1,10 @@
 # Made by Helio <insign@gmail.com> to One Focus
 # Copy to me: powershell.exe -file D:\Dropbox\Sites\onefocus\autobuild.ps1
 
-$git_path = "C:\Users\Helio\AppData\Local\GitHub\PortableGit_93e8418133eb85e81a81e5e19c272776524496c6\cmd\git.exe"
+#$git_path = "C:\Users\Helio\AppData\Local\GitHub\PortableGit_93e8418133eb85e81a81e5e19c272776524496c6\cmd\git.exe"
+$git_path = "git"
 $project_folder  = "D:\Dropbox\Sites\onefocus"
+$git_branch = "master"
 $url_after = "https://build.phonegap.com/apps/270320/push"
 
 ################################################################################
@@ -16,6 +18,9 @@ $commit_msg = [Microsoft.VisualBasic.Interaction]::InputBox("What's the news?", 
 
 # Open the folder
 cd $project_folder
+
+# Select the branch
+$(Invoke-Expression "$git_path checkout $git_branch")
 
 # Add all new files and remove inexistents
 $(Invoke-Expression "$git_path add -A -v")
