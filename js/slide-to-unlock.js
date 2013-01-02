85371237dddf5fa1a84e8da2a3e9430ad37fc58d
Helio Oliveira <insign@gmail.com>
2013-01-02 02:07:45 -0300

    update for build test

--- js/slide-to-unlock.js
+++ js/slide-to-unlock.js
@@ -122,7 +122,7 @@ function stu_touch_way() {
     curX = touch.pageX - this.offsetLeft - 30;
     if (curX > slide_to_unlock_reach_limit()) {
 	 swipe_event('reached');
-	 this.style.webkitTransition = '-webkit-transform 0.5s ease-in';
+	 $(this).hide();
 	 this.style.webkitTransform = 'translateX(0px)';
     } else if (curX > 0) {
 	 el.style.webkitTransform = 'translateX(' + curX + 'px)';
@@ -132,6 +132,7 @@ function stu_touch_way() {
   }, false);
 
   $slider[0].addEventListener('touchend', function(event) {
+    $(this).show();
     this.style.webkitTransition = '-webkit-transform 0.5s ease-in';
     this.addEventListener('webkitTransitionEnd', function(event) {
 	 this.style.webkitTransition = 'none';
