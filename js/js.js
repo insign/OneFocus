68a3585c0d06ccbd47ad68d4435569c90004e8f2
Helio Oliveira <insign@gmail.com>
2013-04-28 01:19:57 -0300

    update for build test

--- js/js.js
+++ js/js.js
@@ -2,69 +2,70 @@
 in_cordova = false;
 
 jQuery(document).ready(function($) {
-  _ = {
-    body: $('body'),
-    wind: $(window),
-    doc: $(document)
-  };
+    _ = {
+        body: $('body'),
+        wind: $(window),
+        doc: $(document)
+    };
 
-  of_events.resize();
-  _.wind.on('resize', of_events.resize);
+    of_events.resize();
+    _.wind.on('resize', of_events.resize);
 
-  of_events.deviceready();
-  _.doc.on('deviceready', of_events.deviceready);
+    of_events.deviceready();
+    _.doc.on('deviceready', of_events.deviceready);
 
-  $('.debug').text(document.URL);
+    $('.debug').text(document.URL);
 });
 
 of_events = {
-  resize: function() {
-    _.body.css({
-	 height: _.body.height()
-    });
-  },
-  deviceready: function() {
-    try {
-	 in_cordova = device;
-	 console.log('Device ready!');
-    } catch (err) {
-	 console.log('Web ready!');
-    }
+    resize: function() {
+        _.body.css({
+            height: _.body.height()
+        });
+    },
+    deviceready: function() {
+        try {
+            in_cordova = device;
+            console.log('Device ready!');
+        } catch (err) {
+            console.log('Web ready!');
+        }
 
-    // Request a play list
-    of_playlist.request();
-  }
+        // Request a play list
+        of_playlist.request();
+    }
 };
 
 of_ajax = {
-  get: function(json, query_string) {
-    var _default_data = {api_version: 1, test: 'a'};
-    var _default = {
-	 // @TODO Send cordava data
-	 url: 'test.json',
-	 dataType: 'json',
-	 cache: false,
-	 data: $.extend(_default_data, query_string),
-	 success: function(data) {
-	   this.success(data);
-	 }.bind(this)
-    };
+    get: function(json, query_string) {
+        var _default_data = {api_version: 1, test: 'a'};
+        var _default = {
+            // @TODO Send cordava data
+            // @TODO Error event
+            url: 'test.json',
+            dataType: 'json',
+            cache: false,
+            data: $.extend(_default_data, query_string),
+            success: function(data) {
+                this.success(data);
+            }.bind(this)
+        };
 
-    var settings = $.extend(_default, json);
+        var settings = $.extend(_default, json);
 
 
-    $.ajax(settings);
-  },
-  success: function(data) {
-    console.log('AJAX received with success', data);
-    $.each(data, function(i, j) {
-	 switch (i) {
-	   case 'playlist':
-		console.log('Playlist received via AJAX', j);
-		of_playlist.register(j);
-		break;
-	 }
-    });
-    console.log('Fim do each');
-  }
+        $.ajax(settings);
+    },
+    success: function(data) {
+        console.log('AJAX received with success', data);
+        $.each(data, function(i, j) {
+            switch (i) {
+                case 'playlist':
+                    console.log('Playlist received via AJAX', j);
+                    of_playlist.register(j);
+                    break;
+            }
+        });
+        console.log('Fim do each');
+    }
 };
