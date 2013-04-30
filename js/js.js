98bf4451151980d68c44d197686d19fde12323af
Helio Oliveira <insign@gmail.com>
2013-04-30 11:47:30 -0300

    update for build test

--- js/js.js
+++ js/js.js
@@ -1,5 +1,12 @@
 // @TODO Cordava ondeviceready & cordova_in_use
 in_cordova = false;
+in_dev = true;
+
+if (!in_dev) {
+    _lol = function() {
+    };
+    this.console = {log: _lol, info: _lol, error: _lol, warn: _lol};
+}
 
 jQuery(document).ready(function($) {
     _ = {
@@ -7,16 +14,12 @@ jQuery(document).ready(function($) {
         wind: $(window),
         doc: $(document)
     };
-
     of_events.resize();
     _.wind.on('resize', of_events.resize);
-
     of_events.deviceready();
     _.doc.on('deviceready', of_events.deviceready);
-
     $('.debug').text(document.URL);
 });
-
 of_events = {
     resize: function() {
         _.body.css({
@@ -31,17 +34,16 @@ of_events = {
             console.log('Web ready!');
         }
 
-        // Request a play list
+// Request a play list
         of_playlist.request();
     }
 };
-
 of_ajax = {
     get: function(json, query_string) {
         var _default_data = {api_version: 1, test: 'a'};
         var _default = {
-            // @TODO Send cordava data
-            // @TODO Error event
+// @TODO Send cordava data
+// @TODO Error event
             url: 'test.json',
             dataType: 'json',
             cache: false,
@@ -50,10 +52,7 @@ of_ajax = {
                 this.success(data);
             }.bind(this)
         };
-
         var settings = $.extend(_default, json);
-
-
         $.ajax(settings);
     },
     success: function(data) {
@@ -69,9 +68,11 @@ of_ajax = {
         console.log('Fim do each');
     }
 };
-
 of_settings = {
     open: function() {
         _.body.addClass('in_settings');
+    },
+    close: function() {
+        _.body.removeClass('in_settings');
     }
 };
--- js/player.js
+++ js/player.js
@@ -1,91 +1,91 @@
 
 of_player = {
-  play: function(json) {
-    if (json) {
+    play: function(json) {
+        if (json) {
+            this.stop();
 
-	 this.stop();
+            this.set_about(json);
 
-	 this.set_about(json);
-
-	 console.info('Sending URL to play now', json.url);
-	 cordova_media.play(json.url);
+            console.info('Sending URL to play now', json.url);
+            if (in_cordova) {
+                cordova_media.play(json.url);
+            }
+        }
+        else {
+            cordova_media.play();
+        }
+    },
+    pause: function() {
+        cordova_media.pause();
+    },
+    stop: function() {
+        cordova_media.stop();
+    },
+    next: function() {
+        of_playlist.use_one();
+    },
+    set_about: function(json) {
+        console.log('Setting the text of author', json);
+        $('.about .name').html(json.name);
+        $('.about .author').html(json.author);
+    },
+    set_status: function(s) {
+        switch (s) {
+            case 1: // Starting/loading
+                this.set_status_class('waiting');
+                break;
+            case 2:
+                this.set_status_class('playing');
+                break;
+            case 3:
+                this.set_status_class('paused');
+                break;
+            default:
+                this.set_status_class('stopped');
+                console.log('Song stopped, trying to play next song');
+                of_player.next();
+                break;
+        }
+    },
+    set_status_class: function(s) {
+        $('body')
+                .removeClass('waiting playing paused stopped')
+                .addClass(s);
     }
-    else {
-	 cordova_media.play();
-    }
-  },
-  pause: function() {
-    cordova_media.pause();
-  },
-  stop: function() {
-    cordova_media.stop();
-  },
-  next: function() {
-    of_playlist.use_one();
-  },
-  set_about: function(json) {
-    console.log('Setting the text of author', json);
-    $('.about .name').html(json.name);
-    $('.about .author').html(json.author);
-  },
-  set_status: function(s) {
-    switch (s) {
-	 case 1: // Starting/loading
-	   this.set_status_class('waiting');
-	   break;
-	 case 2:
-	   this.set_status_class('playing');
-	   break;
-	 case 3:
-	   this.set_status_class('paused');
-	   break;
-	 default:
-	   this.set_status_class('stopped');
-	   console.log('Song stopped, trying to play next song');
-	   of_player.next();
-	   break;
-    }
-  },
-  set_status_class: function(s) {
-    $('body')
-		  .removeClass('waiting playing paused stopped')
-		  .addClass(s);
-  }
 
 };
 of_playlist = {
-  list: {},
-  use_one: function() { // Play a random song of the list or request a new list
-    if (this.list.length > 1) {
-	 console.info('use_one() - Exist items at playlist', this.list);
-	 var random_index = Math.floor(Math.random() * this.list.length);
-	 var random_item = this.list[random_index];
+    list: {},
+    use_one: function() { // Play a random song of the list or request a new list
 
+        console.info('use_one() - Exist items at playlist', this.list);
+        var random_index = Math.floor(Math.random() * this.list.length);
+        var random_item = this.list[random_index];
 
-	 // Select a random item
-	 console.log('Selected random item of playlist to play', random_item);
-	 of_player.play(random_item);
+        // Select a random item
+        console.log('Selected random item of playlist to play', random_item);
+        of_player.play(random_item);
 
-	 // Remove the song used to play
-	 this.list.splice($.inArray(random_index, this.list), 1);
-	 console.info('Removed the item used to play', this.list);
+        // Remove the song used to play
+        this.list.splice(random_index, 1);
+        console.info('Removed the item used to play. Actual is', this.list);
+
+        if (this.list.length < 2) {
+            console.log('*** No items at playlist, request a new playlist...');
+            this.request();
+        }
+    },
+    register: function(json) { // Register the new list then play a new one
+        this.list = json;
+        console.log('Playlist registered', json);
+        console.log('Will use_one() after playlist registered');
+        this.use_one();
+    },
+    request: function() {
+        // @TODO show "loading" and hide it when ready
+
+        var req_data = {send_playlist: 1};
+        console.log('Requesting a playlist', req_data);
+        of_ajax.get(null, req_data);
     }
-    else {
-	 console.log('No items at playlist, request a new playlist...');
-	 this.request();
-    }
-  },
-  register: function(json) { // Register the new list then play a new one
-    this.list = json;
-    console.log('Playlist registered', json);
-    console.log('Will use_one() after playlist registered');
-    this.use_one();
-  },
-  request: function() {
-    // @TODO show "loading" and hide it when ready
-
-    var req_data = {send_playlist: 1};
-    console.log('Requesting a playlist', req_data);
-    of_ajax.get(null, req_data);
-  }
 };
